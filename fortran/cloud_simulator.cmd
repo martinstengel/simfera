6d8043f1b5031d628493490ef8e3df267750981e
Dr. Cornelia Schlundt <cornelia.schlundt@dwd.de>
2016-03-18 09:00:32 +0000

    qsub script

---
+++ fortran/cloud_simulator.cmd
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#PBS -N cloud_simulator
+#PBS -q ns
+#PBS -m e
+#PBS -M dec4@ecmwf.int
+#PBS -l walltime=24:00:00
+#PBS -l EC_ecfs=0
+#PBS -l EC_mars=0
+#PBS -l EC_total_tasks=1
+#PBS -l EC_threads_per_task=1
+#PBS -l EC_memory_per_task=1024mb
+#PBS -o /perm/ms/de/sf7/cschlund/cloud_simulator/fortran/log_cloud_simulator.out
+#PBS -e /perm/ms/de/sf7/cschlund/cloud_simulator/fortran/log_cloud_simulator.err
+
+set -x
+/perm/ms/de/sf7/cschlund/cloud_simulator/fortran/cloud_simulator
+
