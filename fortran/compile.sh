43b29860abaf103e8649a02ec00195835719907f
Dr. Cornelia Schlundt <cornelia.schlundt@dwd.de>
2016-06-28 07:22:44 +0000

    compilation of executable with right modules, finally working

---
+++ fortran/compile.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+module switch cdo cdo/1.6.4
+module switch cray-netcdf netcdf4
+module swap craype-network-aries craype-network-none
+# cray-hdf5 verlinkt libpmi.so.0 und mehr
+module unload cray-hdf5
+module unload cray-mpich
+module unload atp
+module unload pmi
+module list
+make clean
+make
