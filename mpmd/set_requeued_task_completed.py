fea9320ab550ecab1974d7d8e7d1b9a649994c36
Dr. Cornelia Schlundt <cornelia.schlundt@dwd.de>
2017-01-09 12:10:04 +0000

    new python scripts to reset ecflow tasks automatically

---
+++ mpmd/set_requeued_task_completed.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python2.7
+
+# NOTE: before executing this script, you have to: 
+#   source ../config.sh 
+#   for having the right ECF_NODE and ECF_PORT
+
+import ecflow
+
+task_list = ['get_era_data']
+
+client = ecflow.Client()
+client.sync_local()
+defs = client.get_defs()
+tasks = defs.get_all_tasks()
+
+for task in tasks:
+    t = task.get_abs_node_path()
+    tt = t.split("/")[-1]
+    if tt in task_list: 
+        if task.get_state() == ecflow.DState.queued:
+            print " ** Working on: ", task.get_abs_node_path(), task.get_state()
+            client.force_state(task.get_abs_node_path(), ecflow.State.complete)
+
